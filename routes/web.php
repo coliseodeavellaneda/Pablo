82f9e29f92c0942a7a90735aa60dd250a60aa398
coliseodeavellaneda <coliseodeavellaneda@hotmail.com>
2023-07-05 19:01:46 -0300

    comienzo de admin de 0

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::prefix('admin')->middleware('auth')->group(function () {
-    Route::redirect('/', '/admin/product');
-    Route::resource('product', ProductController::class);
-    Route::resource('category', CategoryController::class);
-    Route::resource('role', RoleController::class);
-    Route::resource('permission', PermissionController::class);
-    Route::resource('user', UserController::class)->middleware('accessrole');
-});
+
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
